026226a74f591d78c67a7b7a87c98a76dc9f25c4
boaz.ben-dov <boaz.ben-dov@campus.technion.ac.il>
2020-08-14 16:54:17 +0300

    Implemented fixes, added vocab saving, gitignore.

--- data_handling.py
+++ data_handling.py
@@ -1,47 +1,49 @@
 import re
 from pathlib import Path
 import os
+import json
 
 import pandas as pd
 
-from data_constants import RAW_SIGNS, DOUBLE_SIGNS, SIGN2IDX, LEGARMEI, N_ROWS_PENTACOST
+import data_constants
+from data_constants import RAW_SIGNS, DOUBLE_SIGNS, LEGARMEI, N_ROWS_PENTACOST, DIFFERENT_MEANING_PAIRS
 from utils import init_logger
 
 LOGGER = None
 
 
 def convert_word_to_signs(word):
-    # Only keeping the signs
-    signs = ''.join((filter(lambda c: c in RAW_SIGNS, word)))
     total_duplications_found = 0
 
-    # Removing double signs
-    for sign in DOUBLE_SIGNS:
-        signs, dups = re.subn(f'{sign}{sign}', sign, signs)
-        total_duplications_found += dups
+    for pair, result in DIFFERENT_MEANING_PAIRS.items():
+        if all(item in word for item in pair):
+            word = list(filter(lambda x: x not in pair, word))
+            word.append(result)
+            total_duplications_found += 1
 
-    signs = list(map(SIGN2IDX.__getitem__, signs))
-
-    if SIGN2IDX[chr(0x05A3)] in signs and chr(0x05C0) in word:
-        # Munach + Paseq
-        signs.remove(SIGN2IDX[chr(0x05A3)])
-        signs.append(SIGN2IDX[LEGARMEI])
-        total_duplications_found += 1
-
-    # More exceptions to take care of:
-    # - When Pashta appear twice, the first appearance is Kadma
-    # - When Zarka appears twice, it does so as two different signs
-    # - Generalize the above two cases together with the Munach + Paseq case.
-    # - Collapse the two signs for Geresh (11 + 12) into a single one.
+    # Only keeping the signs
+    signs = list(map(data_constants.SIGN2IDX.__getitem__,
+                     filter(lambda c: c in RAW_SIGNS, word)))
 
     LOGGER.info(f'Replaced {total_duplications_found} signs (double or special cases).')
 
     return signs
 
 
+def remove_metagim(verse):
+    sof_passuk = data_constants.SIGN2IDX[chr(0x05BD)]
+    verse = list(filter(lambda x: x != sof_passuk, verse))
+    verse.append(sof_passuk)
+    return verse
+
+
 def create_teamim_df(config):
     path = Path(config['raw_path'])
     output_dir = Path(config['output_dir'])
+
+    if (output_dir / 'vocab.json').is_file():
+        with open(output_dir / 'vocab.json', 'r') as f:
+            data_constants.SIGN2IDX = json.load(f)
 
     # Names to give to the columns
     columns = ['book', 'chapter', 'verse', 'raw_word']
@@ -63,11 +65,15 @@ def create_teamim_df(config):
     # Group by the distinct verses and concatenate it all together
     verse_groups = df.groupby(by=['book', 'chapter', 'verse'])
     df = verse_groups.signs.aggregate(lambda l: [item for sublist in l for item in sublist]).reset_index()
+    df['signs'] = df['signs'].apply(remove_metagim)
     # df = pd.DataFrame(df.to_records())
 
     if config.get('save_verses', False):
         output_dir.mkdir(exist_ok=True, parents=True)
         df.to_csv(output_dir / f'parsed_verses.csv', index_label='index', index=True, header=True)
+        if not (output_dir / 'vocab.json').is_file():
+            with open(output_dir / 'vocab.json', 'w+') as f:
+                json.dump(data_constants.SIGN2IDX, f)
     return df
 
 
@@ -92,7 +98,7 @@ if __name__ == '__main__':
         'save_verses': True
     }
 
-    print(SIGN2IDX)
+    print(data_constants.SIGN2IDX)
 
     df = get_teamim_df(config)
     print(df[df['book'] == 'Gen'].head())
